eb4e5bc6eb2f8306dc387082965eda24475dea9e
planet27 <yhanazono0227@gmail.com>
2024-12-03 08:29:35 +0900

    キャッシュ対策を追記

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-1-todo/'
-})
+  base: '/portfolio-1-todo/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[hash].[ext]', // CSSや画像などのアセットファイル
+        chunkFileNames: '[name].[hash].js',   // 動的インポートのJSファイル
+        entryFileNames: '[name].[hash].js',   // エントリーポイントのJSファイル
+      },
+    },
+  },
+});

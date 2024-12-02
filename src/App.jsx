24e6414aede5e35ecd53012704621a628cd9d960
planet27 <yhanazono0227@gmail.com>
2024-12-02 09:07:17 +0900

    最初のコミット

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from './component/Todo'
+
+import './App.css'
+
+const App = () => {
+ 
+  return (
+    <>
+      <Todo />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/List.jsx
@@ -0,0 +1,16 @@
+import { useTodos } from "../context/TodoContext";
+import Item from "./Item";
+
+const List = () => {
+  const todos = useTodos();
+
+  return (
+    <>
+      {todos.map((todo) => {
+        return <Item key={todo.id} todo={todo}/>;
+      })}
+    </>
+  );
+};
+
+export default List;

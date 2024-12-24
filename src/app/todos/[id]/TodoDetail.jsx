1629787e4e2edbb2007b285bd4dc714834c7c349
planet27 <yhanazono0227@gmail.com>
2024-12-24 15:04:50 +0900

    個別にtodoを追加できる機能を実装

--- src/app/todos/[id]/TodoDetail.jsx
+++ src/app/todos/[id]/TodoDetail.jsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
-import { useTodos, useTodosDispatch } from "@/context/TodoContext";
+import { use } from "react";
+import { useTodos } from "@/context/TodoContext";
+import { useRouter } from "next/navigation";
 import TodoDetailItem from "./TodoDetailItem";
 import TodoDetailForm from "./TodoDetailForm";
 
 export default function TodoDetail({ params }) {
   const { id } = use(params); // paramsからidを取得
+  const router = useRouter();
+
   const todos = useTodos();
   const foundList = todos.find(
     (todoList) => todoList.id === id
   );
   console.log(todos);
   if (todos.length === 0) {
-    return <div>Todoリストを読み込んでいます...</div>;
+    return (
+    <div>Todoリストを読み込んでいます...</div>
+  );
   }
 
   if (!foundList) {
@@ -22,13 +27,19 @@ export default function TodoDetail({ params }) {
 
 
   return (
-    <div className="p-4">
+    <div className="fixed top-0 right-0 w-1/3 h-full p-6 bg-white shadow-lg">
+      <button
+        onClick={() => router.push("/")} // 編集ページを閉じる
+        className="p-2 mt-4 text-white bg-blue-500 rounded"
+      >
+        閉じる
+      </button>
       <h1 className="text-2xl font-bold">{foundList.title}</h1>
       <p>作成した日付: {foundList.date}</p>
       {foundList.todos.map((todo) => (
         <TodoDetailItem key={todo.id} todo={todo} />
       ))}
-      <div className="fixed w-full max-w-md p-6 transform -translate-x-1/2 bg-white rounded-lg shadow-md bottom-6 left-1/2 sw-full">
+      <div className="fixed w-full max-w-md p-6 transform bg-white rounded-lg shadow-md -translate-x-1/3 bottom-6 left-1/2 sw-full">
         <TodoDetailForm listId={id} />
       </div>
     </div>

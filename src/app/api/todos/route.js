44761df4174445d7068e83b7dce925e9a87989cd
planet27 <yhanazono0227@gmail.com>
2025-04-28 16:39:59 +0900

    vercelでのビルド用に環境変数を修正

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../../../firebase-adminsdk.json";
 
-// Firebase Admin SDK の初期化（すでに初期化済みの場合はスキップ）
+// Firebase Admin 初期化
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
